a387c0d5a24bce6136a1034b78e2cfa5c361ea5a
Iskander Sierra <iskander.sierra@gmail.com>
2020-04-18 12:41:09 +0200

    Moved grpc client to its own library

---
+++ KVStore.Client/ServiceWrapper.fs
@@ -0,0 +1,57 @@
+﻿namespace PolyCoder.KVStore.Client
+
+open PolyCoder
+open PolyCoder.KVStore.Abstractions
+open PolyCoder.KVStore.Protobuf
+open Google.Protobuf
+open Grpc.Core
+
+[<AutoOpen>]
+module ServiceWrapper =
+  let createKeyValueStore
+    (service: KeyValueStoreService.KeyValueStoreServiceClient)
+    : KeyValueStore<byte[], byte[]> =
+    
+    let inline ofBytes (bytes: byte[]) = ByteString.CopyFrom(bytes)
+    let inline toBytes (bytes: ByteString) = bytes.ToByteArray()
+
+    let callOptions() = async {
+      let! cancel = Async.CancellationToken
+      return CallOptions(cancellationToken = cancel)
+    }
+
+    let store key value = async {
+      let request = StoreRequest()
+      request.Key <- ofBytes key
+      request.Value <- ofBytes value
+
+      let! options = callOptions()
+
+      let! _reply = service.StoreAsync(request, options).ResponseAsync |> Async.AwaitTask
+      return ()
+    }
+
+    let remove key = async {
+      let request = RemoveRequest()
+      request.Key <- ofBytes key
+
+      let! options = callOptions()
+
+      let! _reply = service.RemoveAsync(request, options).ResponseAsync |> Async.AwaitTask
+      return ()
+    }
+
+    let retrieve key = async {
+      let request = RetrieveRequest()
+      request.Key <- ofBytes key
+
+      let! options = callOptions()
+
+      let! reply = service.RetrieveAsync(request, options).ResponseAsync |> Async.AwaitTask
+      if reply.Found then
+        return Some (toBytes reply.Value)
+      else
+        return None
+    }
+
+    KeyValueStore.assemble store remove retrieve
--- KVStore.Protobuf.ServiceImpl/KeyValueStore.fs
+++ KVStore.Protobuf.ServiceImpl/KeyValueStore.fs
@@ -4,58 +4,7 @@ open PolyCoder
 open PolyCoder.KVStore.Abstractions
 open PolyCoder.KVStore.Protobuf
 open Google.Protobuf
-open Grpc.Core
 open FSharp.Control.Tasks.V2
-
-[<AutoOpen>]
-module ServiceWrapper =
-  let createKeyValueStoreWrapper
-    (service: KeyValueStoreService.KeyValueStoreServiceClient)
-    : KeyValueStore<byte[], byte[]> =
-    
-    let inline ofBytes (bytes: byte[]) = ByteString.CopyFrom(bytes)
-    let inline toBytes (bytes: ByteString) = bytes.ToByteArray()
-
-    let callOptions() = async {
-      let! cancel = Async.CancellationToken
-      return CallOptions(cancellationToken = cancel)
-    }
-
-    let store key value = async {
-      let request = StoreRequest()
-      request.Key <- ofBytes key
-      request.Value <- ofBytes value
-
-      let! options = callOptions()
-
-      let! _reply = service.StoreAsync(request, options).ResponseAsync |> Async.AwaitTask
-      return ()
-    }
-
-    let remove key = async {
-      let request = RemoveRequest()
-      request.Key <- ofBytes key
-
-      let! options = callOptions()
-
-      let! _reply = service.RemoveAsync(request, options).ResponseAsync |> Async.AwaitTask
-      return ()
-    }
-
-    let retrieve key = async {
-      let request = RetrieveRequest()
-      request.Key <- ofBytes key
-
-      let! options = callOptions()
-
-      let! reply = service.RetrieveAsync(request, options).ResponseAsync |> Async.AwaitTask
-      if reply.Found then
-        return Some (toBytes reply.Value)
-      else
-        return None
-    }
-
-    KeyValueStore.assemble store remove retrieve
 
 module ServiceImpl =
   type KeyValueStoreServiceImplOptions = {
